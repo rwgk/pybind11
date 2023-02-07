8a80b580d96298b678391b1428d216265a2c7713
Ralf W. Grosse-Kunstleve <rwgk@google.com>
2023-02-06 21:39:46 -0800

    test_return_nested_pair_string

--- tests/test_return_value_opts.cpp
+++ tests/test_return_value_opts.cpp
@@ -6,25 +6,44 @@
 #include <utility>
 
 namespace {
-std::pair<std::string, std::string> return_pair_string() {
-    return std::pair<std::string, std::string>({"", ""});
+
+using PairString = std::pair<std::string, std::string>;
+
+PairString return_pair_string() { return PairString({"", ""}); }
+
+using NestedPairString = std::pair<PairString, PairString>;
+
+NestedPairString return_nested_pair_string() {
+    return NestedPairString(return_pair_string(), return_pair_string());
 }
+
 } // namespace
 
 TEST_SUBMODULE(return_value_opts, m) {
+    auto rvpc = py::return_value_policy::_clif_automatic;
+    auto rvpb = py::return_value_policy::_return_as_bytes;
+
     m.def("return_pair_str_str", []() { return return_pair_string(); });
     m.def(
-        "return_pair_bytes_bytes",
-        []() { return return_pair_string(); },
-        py::return_value_policy::_return_as_bytes);
+        "return_pair_bytes_bytes", []() { return return_pair_string(); }, rvpb);
     m.def(
         "return_pair_str_bytes",
         []() { return return_pair_string(); },
-        py::return_value_opts({py::return_value_policy::_clif_automatic,
-                               py::return_value_policy::_return_as_bytes}));
+        py::return_value_opts({rvpc, rvpb}));
     m.def(
         "return_pair_bytes_str",
         []() { return return_pair_string(); },
-        py::return_value_opts({py::return_value_policy::_return_as_bytes,
-                               py::return_value_policy::_clif_automatic}));
+        py::return_value_opts({rvpb, rvpc}));
+
+    m.def("return_nested_pair_str", []() { return return_nested_pair_string(); });
+    m.def(
+        "return_nested_pair_bytes", []() { return return_nested_pair_string(); }, rvpb);
+    m.def(
+        "return_nested_pair_str_bytes_bytes_str",
+        []() { return return_nested_pair_string(); },
+        py::return_value_opts({{rvpc, rvpb}, {rvpb, rvpc}}));
+    m.def(
+        "return_nested_pair_bytes_str_str_bytes",
+        []() { return return_nested_pair_string(); },
+        py::return_value_opts({{rvpb, rvpc}, {rvpc, rvpb}}));
 }
--- tests/test_return_value_opts.py
+++ tests/test_return_value_opts.py
@@ -12,6 +12,21 @@ from pybind11_tests import return_value_opts as m
         (m.return_pair_bytes_str, (bytes, str)),
     ],
 )
-def test_return_pair_bytes_bytes(func, expected):
-    tup = func()
-    assert all(isinstance(e, t) for e, t in zip(tup, expected))
+def test_return_pair_string(func, expected):
+    ps = func()
+    assert all(isinstance(e, t) for e, t in zip(ps, expected))
+
+
+@pytest.mark.parametrize(
+    "func, expected",
+    [
+        (m.return_nested_pair_str, (str, str, str, str)),
+        (m.return_nested_pair_bytes, (bytes, bytes, bytes, bytes)),
+        (m.return_nested_pair_str_bytes_bytes_str, (str, bytes, bytes, str)),
+        (m.return_nested_pair_bytes_str_str_bytes, (bytes, str, str, bytes)),
+    ],
+)
+def test_return_nested_pair_string(func, expected):
+    nps = func()
+    assert len(nps) == 2
+    assert all(isinstance(e, t) for e, t in zip(nps[0] + nps[1], expected))
